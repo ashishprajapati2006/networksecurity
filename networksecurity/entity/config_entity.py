14cdf7bee8cabe8cc80f9ace240d3a4a3a264ae1
ashishprajapati2006 <ashishofficial12321@gmail.com>
2025-12-25 23:53:10 +0530

    Data ingestion component completed

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -5,23 +5,26 @@ from networksecurity.constant import training_pipeline
 print(training_pipeline.PIPELINE_NAME)
 print(training_pipeline.ARTIFACT_DIR)
 
-class TrainingPipelineConfig:
-    def _init__(self, timestap = datetime.now()):
-        timestap = timestap.strftime("%m_%d_%Y_%H_%M_%S")
-        self.pipeline_name = training_pipeline.PIPELINE_NAME
-        self.artifact_name = training_pipeline.ARTIFACT_DIR
-        self.artifact_dir = os.path.join(self.artifact_name,timestap)
-        self.model_dir = os.path.join("final_maodel")
-        self.timestamp: str= timestap
 
-class DataIngestionConfig:
-    def __init__(self, training_pipeline_config: TrainingPipelineConfig):
-        self.data_ingestion_dir: str= os.path.join(
-            training_pipeline_config.artifact_dir, training_pipeline.DATA_INGESTION_DIR_NAME
+class TrainingPipelineConfig: 
+    def __init__(self,timestamp=datetime.now()):
+        timestamp=timestamp.strftime("%m_%d_%Y_%H_%M_%S")
+        self.pipeline_name=training_pipeline.PIPELINE_NAME
+        self.artifact_name=training_pipeline.ARTIFACT_DIR
+        self.artifact_dir=os.path.join(self.artifact_name,timestamp)
+        self.model_dir=os.path.join("final_model")
+        self.timestamp: str=timestamp
+
+
+
+class DataIngestionConfig: 
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_ingestion_dir:str=os.path.join(
+            training_pipeline_config.artifact_dir,training_pipeline.DATA_INGESTION_DIR_NAME
         )
-        self.features_store_file_path: str= os.path.join(
-            self.data_ingestion_dir , training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR, training_pipeline.FILE_NAME
-        )
+        self.feature_store_file_path: str = os.path.join(
+                self.data_ingestion_dir, training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR, training_pipeline.FILE_NAME
+            )
         self.training_file_path: str = os.path.join(
                 self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR, training_pipeline.TRAIN_FILE_NAME
             )
