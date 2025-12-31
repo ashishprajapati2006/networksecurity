00f7333905f56ebdf6a4a3a27e791f9b86a8c928
ashishprajapati2006 <ashishofficial12321@gmail.com>
2025-12-31 23:33:54 +0530

    pipeline completed

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -124,7 +124,7 @@ class ModelTrainer:
         os.makedirs(model_dir_path,exist_ok=True)
 
         Network_Model=NetworkModel(preprocessor=preprocessor,model=best_model)
-        save_object(self.model_trainer_config.trained_model_file_path,obj=Network_Model)
+        save_object(self.model_trainer_config.trained_model_file_path,obj=NetworkModel)
 
         #model pusher
         save_object("final_model/model.pkl",best_model)
